3ddac3040ec39c4f7ea8f46a59bed3be593f6acc
Joshua Si <joshuasi777@gmail.com>
2023-04-30 01:13:09 -0700

    WIP chunking

--- include/TileMap.h
+++ include/TileMap.h
@@ -5,15 +5,21 @@
 class TileMap : public sf::Drawable, public sf::Transformable
 {
 public:
-    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+    bool loadTileset(const std::string& tileset, sf::Vector2u tileSize);
+    void loadMap(const int* tiles, unsigned int mapWidth, unsigned int mapHeight, sf::View view);
+    void loadVertexChunk(sf::Vector2f view_coord);
+    void flash(sf::Vector2f v);
 
 private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-    sf::VertexArray m_vertices;  // The vertex array used to draw the tiles
+    sf::VertexArray m_vertices;  // The vertex array to draw the tiles onto the view
+    sf::Vector2u m_chunkSize;    // The dimensions of a VertexArray chunk when rendered (in tiles)
     sf::Texture m_tileset;       // The tileset texture
-    sf::Vector2u m_tileSize;     // The size of each tile
-    sf::Vector2u m_mapSize;      // The size of the tile map
+    sf::Vector2u m_tileSize;     // The dimensions of each tile (in pixels)
+    std::vector<int> m_tiles;    // The tile map
+    sf::Vector2u m_mapSize;      // The dimensions of the entire tile map (in tiles)
+    sf::Vector2f m_center;       // The center of the view since loadVertexChunk is last called
 };
 
 #endif
--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -1,62 +1,92 @@
 #include "TileMap.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <algorithm>
 
-bool TileMap::load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+bool TileMap::loadTileset(const std::string& tileset, sf::Vector2u tileSize)
 {
-  // Load the tileset texture
   if (!m_tileset.loadFromFile(tileset))
     return false;
 
-  // Store the tile size and map size
   m_tileSize = tileSize;
-  m_mapSize.x = width;
-  m_mapSize.y = height;
+  return (m_tileSize != sf::Vector2u(0, 0));
+}
 
-  // Initialize the vertex array
+void TileMap::loadMap(const int* tiles, unsigned int mapWidth, unsigned int mapHeight, sf::View view)
+{
+  m_tiles.resize(mapWidth * mapHeight);
+  for (int i = 0; i < mapWidth * mapHeight; i++)
+    m_tiles[i] = tiles[i];
+
+  m_mapSize = sf::Vector2u(mapWidth, mapHeight);
+  sf::Vector2f chunk = view.getSize() * 1.0f;
+  m_chunkSize = sf::Vector2u(chunk.x / m_tileSize.x, chunk.y / m_tileSize.y);
+
   m_vertices.setPrimitiveType(sf::Quads);
-  m_vertices.resize(width * height * 4);
+  m_vertices.resize(m_chunkSize.x * m_chunkSize.y * 4);
+}
 
+void TileMap::loadVertexChunk(sf::Vector2f view_coord)
+{
+  float chunkLimit_x = m_chunkSize.x * m_tileSize.x / 2;
+  float chunkLimit_y = m_chunkSize.y * m_tileSize.y / 2;
+  float t_x = std::max(view_coord.x - chunkLimit_x, 0.0f);
+  float t_y = std::max(view_coord.y - chunkLimit_y, 0.0f);
+
+  if (t_x >= (m_center.x - chunkLimit_x) && t_x <= (m_center.x + chunkLimit_x)
+    && t_y >= (m_center.y - chunkLimit_y) && t_y <= (m_center.y + chunkLimit_y))
+    {
+      return;
+    }
+
+  m_center = view_coord;
+
+  std::cout << t_x << " " << (m_center.x - chunkLimit_x) << ", " << t_y << " " << (m_center.y - chunkLimit_y) << std::endl;
+  std::cout << "c" << m_center.x << " " << m_center.y << std::endl;
+
+  // flash(sf::Vector2f(t_x / m_tileSize.x, t_y / m_tileSize.y));
+}
+
+void TileMap::flash(sf::Vector2f t)
+{
   // Populate the vertex array, with one quad per tile
-  for (unsigned int i = 0; i < width; ++i)
+  for (unsigned int i = t.x; i < m_chunkSize.x + t.x; i++)
   {
-    for (unsigned int j = 0; j < height; ++j)
+    for (unsigned int j = t.y; j < m_chunkSize.y + t.y; j++)
     {
       // Get the current tile number
-      int tileNumber = tiles[i + j * width];
+      int tileNumber = m_tiles[i + j * m_chunkSize.x];
 
       // Calculate its position in the tileset texture
-      int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-      int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+      int tu = tileNumber % (m_tileset.getSize().x / m_tileSize.x);
+      int tv = tileNumber / (m_tileset.getSize().x / m_tileSize.x);
 
       // Get a pointer to the current tile's quad
-      sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+      sf::Vertex* quad = &m_vertices[(i + j * m_chunkSize.x) * 4];
 
       // Define corners
-      quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-      quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-      quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-      quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+      quad[0].position = sf::Vector2f(i * m_tileSize.x, j * m_tileSize.y);
+      quad[1].position = sf::Vector2f((i + 1) * m_tileSize.x, j * m_tileSize.y);
+      quad[2].position = sf::Vector2f((i + 1) * m_tileSize.x, (j + 1) * m_tileSize.y);
+      quad[3].position = sf::Vector2f(i * m_tileSize.x, (j + 1) * m_tileSize.y);
 
       // Define texture coordinates
-      quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-      quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-      quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-      quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+      quad[0].texCoords = sf::Vector2f(tu * m_tileSize.x, tv * m_tileSize.y);
+      quad[1].texCoords = sf::Vector2f((tu + 1) * m_tileSize.x, tv * m_tileSize.y);
+      quad[2].texCoords = sf::Vector2f((tu + 1) * m_tileSize.x, (tv + 1) * m_tileSize.y);
+      quad[3].texCoords = sf::Vector2f(tu * m_tileSize.x, (tv + 1) * m_tileSize.y);
     }
   }
-
-  return true;
 }
 
 void TileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    // Combine the TileMap's transform with the current RenderStates
-    states.transform *= getTransform();
+  // Combine the TileMap's transform with the current RenderStates
+  states.transform *= getTransform();
 
-    // Set the TileMap's texture as the current texture in the RenderStates
-    states.texture = &m_tileset;
+  // Set the TileMap's texture as the current texture in the RenderStates
+  states.texture = &m_tileset;
 
-    // Draw the visible tiles to the RenderTarget using the updated RenderStates
-    target.draw(m_vertices, states);
+  // Draw the visible tiles to the RenderTarget using the updated RenderStates
+  target.draw(m_vertices, states);
 }
